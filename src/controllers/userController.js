06696bc562f0e147ccf5af0efb2b8a6f6009a407
thatvirgogurl <mishramonalisha9@gmail.com>
2022-08-30 23:06:00 +0530

    changes

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,22 +2,26 @@ const jwt = require("jsonwebtoken");
 const userModel = require("../models/userModel");
 
 const createUser = async function (abcd, xyz) {
-  //You can name the req, res objects anything.
-  //but the first parameter is always the request 
-  //the second parameter is always the response
+
+  try{
   let data = abcd.body;
   let savedData = await userModel.create(data);
-  console.log(abcd.newAtribute);
-  xyz.send({ msg: savedData });
+      xyz.status(201).send({ msg: savedData })
+  }
+
+catch(err){
+  xyz.status(400).send({ "error 400":err.message});
+}
 };
 
 const loginUser = async function (req, res) {
+    try{
   let userName = req.body.emailId;
   let password = req.body.password;
 
   let user = await userModel.findOne({ emailId: userName, password: password });
   if (!user)
-    return res.send({
+    return res.status(403).send({
       status: false,
       msg: "username or the password is not correct",
     });
@@ -37,40 +41,61 @@ const loginUser = async function (req, res) {
     "functionup-plutonium-very-very-secret-key"
   );
   //res.setHeader("x-auth-token", token);
-  res.send({ status: true, data: token });
+  res.status(200).send({ status: true, data: token });
+}
+    catch (err) {
+        res.status(500).send({ "error 500": err.message });
+    }
 };
 
 const getUserData = async function (req, res) {
-
+try{
   let userId = req.params.userId;
   let userDetails = await userModel.findById(userId);
-  if (!userDetails)
-    return res.send({ status: false, msg: "No such user exists" });
 
-  res.send({ status: true, data: userDetails });
+
+  if (!userDetails)
+
+    return res.status(403).send({ status: false, msg: "No such user exists" });
+
+  res.status(200).send({ status: true, data: userDetails });
+}
+catch (err) {
+    res.status(401).send({ "error 401": err.message });
+}
 };
 
 const updateUser = async function (req, res) {
   let userId = req.params.userId;
+  try{
   let user = await userModel.findById(userId);
   //Return an error if no user with the given id exists in the db
   if (!user) {
-    return res.send("No such user exists");
+    return res.status(403).send("No such user exists");
   }
 
   let userData = req.body;
   let updatedUser = await userModel.findOneAndUpdate({ _id: userId }, userData, {new: true});
-  res.send({ status: true, data: updatedUser });
+  res.status(201).send({ status: true, data: updatedUser });
+}
+  catch (err) {
+      res.status(400).send({ "error 400": err.message });
+  }
 };
 
-const deleteUser = async function(req, res) {    
+const deleteUser = async function(req, res) {  
+    try{  
   let userId = req.params.userId
   let user = await userModel.findById(userId)
   if(!user) {
-      return res.send({status: false, message: "no such user exists"})
+      return res.status(403).send({status: false, message: "no such user exists"})
   }
   let updatedUser = await userModel.findOneAndUpdate({_id: userId}, {isDeleted: true}, {new: true})
-  res.send({status: true, data: updatedUser})
+  res.status(201).send({status: true, data: updatedUser})
+}
+  catch (err) {
+        res.status(400).send({ "error 400": err.message });
+    }
 }
 
 module.exports.createUser = createUser;
--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,32 +1,38 @@
+const { response } = require("express");
 const jwt = require("jsonwebtoken");
 
 const validateToken = function(req, res, next) {
+  try{
     let token = req.headers["x-auth-token"];
     //if (!token) token = req.headers["x-auth-token"];
   
     //If no token is present in the request header return error
-    if (!token) return res.send({ status: false, msg: "token must be present" });
+    if (!token) return res.status(403).send({ status: false, msg: "token must be present" });
   
     console.log(token);
-    
-    // If a token is present then decode the token with verify function
-    // verify takes two inputs:
-    // Input 1 is the token to be decoded
-    // Input 2 is the same secret with which the token was generated
-    // Check the value of the decoded token yourself
+  
     let decodedToken = jwt.verify(token, "functionup-plutonium-very-very-secret-key");
     if (!decodedToken) {
-      return res.send({ status: false, msg: "token is invalid" });
+      return res.status(403).send({ status: false, msg: "token is invalid" });
     }
   req.loggedInUserId = decodedToken.userId
+}
+  catch(err) {
+    res.status(400).send({ "error 400": err.message });
+  }
     next()
 }
 const checkforAuthorization=function(req,res,next)
 {
+  try{
   let requireId = req.params.userId
   if(requireId!==req.loggedInUserId)
   {
-    return res.send({msg:false,msg:"permission denied"})
+    return res.status(401).send({msg:false,msg:"permission denied"})
+  }
+}
+catch(err) {
+    res.status(400).send({ "error 400": err.message });
   }
   next()
 }
