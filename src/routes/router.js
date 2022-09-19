b71442dd936f7cc7c7423b8a5e572f21b7978069
= <=>
2022-09-20 05:15:09 +0530

    chages

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,21 +1,13 @@
 
 const express = require("express")
 const router = express.Router();
-const userController= require("../controllers/userController")
+const userController= require("../controller/userController")
 
-
+//===============================CreateUser=======================================//
 router.post("/register",userController.CreateUser)
 
-module.exports
-const express = require('express')
-const router = express.Router()
-const bookController = require("../controller/bookController")
-
 //------------------------------Registered a book -----------------------------------------------//
-router.post("/books", bookController.createBooks)
-
-
-
+// router.post("/books", bookController.createBooks)
 
 module.exports = router;
->>>>>>> 683a05741b66800887b0a24bb050914c542be283
+
