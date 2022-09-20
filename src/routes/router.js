683a05741b66800887b0a24bb050914c542be283
Gautam <gautamdhakate1234@gmail.com>
2022-09-20 15:42:06 +0530

    Add validator file and fuction createBook in bookControllers

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,3 +1,11 @@
 const express = require('express')
 const router = express.Router()
+const bookController = require("../controller/bookController")
+
+//------------------------------Registered a book -----------------------------------------------//
+router.post("/books", bookController.createBooks)
+
+
+
+
 module.exports = router;
