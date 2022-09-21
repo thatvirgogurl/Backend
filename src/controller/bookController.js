1f3131bf7fb830b8a30472ae8fbc3eff710f99fd
Gautam <gautamdhakate1234@gmail.com>
2022-09-21 16:54:02 +0530

    Update login Api

--- src/controller/bookController.js
+++ src/controller/bookController.js
@@ -21,8 +21,6 @@ const createBooks = async function (req, res) {
 
         if (!isValid(title)) return res.status(400).send({ status: false, msg: "title is required and it's must be string" })
         if (!isValidRegex1(title)) return res.status(400).send({ status: false, msg: "Invalid title" })
-        // if(!/^[a-zA-Z-" "]{2,10}$/.test(title)) return res.status(400).send({ status: false, msg: "Invalid title" })
-        
         
         if (!isValid(excerpt)) return res.status(400).send({ status: false, msg: "excerpt is required and it's must be string" })
         if (!isValidRegex1(excerpt)) return res.status(400).send({ status: false, msg: "Invalid excerpt" })
@@ -65,45 +63,5 @@ const createBooks = async function (req, res) {
         return res.status(500).send({status : false, msg : err.message})
     }
 }
-
-// const createBooks = async function(req,res){
-
-//     const requestBody = req.body
-
-//     if(!isValidRequestBody(requestBody)) return res.status(400).send({ status: false, msg: "No parameter is found ,Plz provide detail" })
-    
-//     let validateRequestBody = ["title","excerpt","userId","ISBN","category"]
-//     const { title, excerpt, userId, ISBN, category, subcategory,reviews} = requestBody
-
-//     for(let element of validateRequestBody){
-//         if(!isValid(requestBody[element])) return res.status(400).send({status : false , mag :`${element} is required and it's must be string`})
-//     }
-
-//     for(let element of validateRequestBody){
-//         if(element == "userId" || element == "ISBN") continue;
-//         else{
-//             if(!isValidRegex1(requestBody[element])) return res.status(400).send({status : false , msg : `invalid ${element}`})
-//         }
-//     }
-
-//     if(!isValidObjectId(userId)) return res.status(400).send({ status: false, msg: "Invalid userId" })
-//     if (!isValidRegex2(ISBN)) return res.status(400).send({ status: false, msg: "Invalid ISBN number" })
-
-//     if (!Array.isArray(subcategory)) return res.status(400).send({ status: false, msg: "category is required and it's must be Array" })
-//     if (!isValidRegex1(subcategory)) return res.status(400).send({ status: false, msg: "Invalid subcategory" }) // check subcategory array or string if array then changes
-
-//     if (reviews) {
-//         if (typeof reviews !== "number") return res.status(400).send({ status: false, msg: "reviews in Nuber type" })
-//         if (!/^[0-9]$/.test(reviews)) return res.status(400).send({ status: false, msg: "Invalid review Number" }) // change in regex
-//     }
-
-//     for(element )
-
-
-
-
- 
-
-// }
 
 module.exports.createBooks = createBooks
