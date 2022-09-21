16962b2008cee3e133371d575e9a55b4d488721f
thatvirgogurl <mishramonalisha9@gmail.com>
2022-09-21 16:19:41 +0530

    changes

--- src/controller/bookController.js
+++ src/controller/bookController.js
@@ -1,7 +1,7 @@
-const bookModel = require("../../models/bookmodel");
+const bookModel = require("../models/bookmodel");
 const userModel = require("../models/usermodel")
 const moment = require("moment")
-const {isValidRequestBody,isValid,isValidRegex1,isValidRegex2,isValidObjectId} = require("../validator/validator")
+const { isValidRequestBody, isValid, isValidRegex1, isValidRegex2, isValidObjectId } = require("../validator/validator")
 
 
 const createBooks = async function (req, res) {
@@ -14,7 +14,7 @@ const createBooks = async function (req, res) {
 
         // destructuring parameter of request body 
 
-        const { title, excerpt, userId, ISBN, category, subcategory,reviews} = requestBody
+        const { title, excerpt, userId, ISBN, category, subcategory, reviews } = requestBody
 
         //----------------------------- Start Validation ----------------------------------------------
 
@@ -50,15 +50,72 @@ const createBooks = async function (req, res) {
         const isUniqueISBN = await bookModel.findOne({ ISBN: ISBN })
         if (isUniqueISBN) return res.status(409).send({ status: false, msg: "ISBN number is already exist" })
 
-        const data = { title, excerpt, userId, ISBN, category, subcategory, releasedAt :moment().format("DD/MM/YYYY , h:mm:ss a"), reviews } // check when i not send released at then what happen
+        const data = { title, excerpt, userId, ISBN, category, subcategory, releasedAt: moment().format("DD/MM/YYYY , h:mm:ss a"), reviews } // check when i not send released at then what happen
 
         const saveData = await bookModel.Create(data)
 
         return res.status(201).send({ status: true, msg: "Book detail is successfully registered", data: saveData })
 
     } catch (err) {
-        return res.status(500).send({status : false, msg : err.message})
+        return res.status(500).send({ status: false, msg: err.message })
     }
 }
 
-module.exports.createBooks = createBooks
+const getallBook = async function (req, res) {
+    try {
+        let { userId, category, subcategory } = req.query
+        let obj = { isDeleted: false }
+        if (userId != null) { obj.userId = userId }
+        if (category != null) { obj.category = category }
+        if (subcategory != null) { obj.subcategory = subcategory }
+        let listOfbook = await bookModel.find(obj).select({ subcategory: 0, ISBN: 0 }).sort({ title: 1 })
+        if (listOfbook.length == 0) {
+            return res.status(404).send({ status: false, msg: "no document found" })
+        }
+        return res.status(200).send({
+            status: true,
+            message: 'Books list', Data: {listOfbook}
+        })
+    }
+    catch (err) {
+        res.status(500).send({
+            status: false,
+            msg: err.message
+        })
+    }
+}
+const updatebooks= async function (req, res) {
+    try {
+        let bookId = req.params.bookId;
+        if (!bookId) return res.status(400).send("bookId is required")
+        validatBookID = mongoose.Types.ObjectId.isValid(bookId)
+        if (!validatBookID) { return res.status(404).send({ status: false, msg: " require a valid Id" }); }
+        let bookDetails = await bookModel.findById(bookId);
+        if (!bookDetails) {
+            return res.status(404).send({ status: false, msg: "no such book  exist" });
+        }
+        let { title, excerpt, ISBN } = req.body;
+        let obj = {}
+        if (title !== null) { obj.title = title }
+        if (excerpt !== null) { obj.excerpt = excerpt }
+        // if (releasedAt !== null) { obj.releasedAt = releasedAt }
+        if (ISBN !== null) { obj.ISBN = ISBN }
+        let updatebook = await blogmodel.findOneAndUpdate({ _id: bookId }, { $set: { obj, releasedAt: moment().format("DD/MM/YYYY , h:mm:ss a") } }, {  new: true })
+        if (updatebook.isDeleted == true) return res.status(404).send({ 
+            status: false,
+            message: "books not found"
+        })
+        res.status(200).send({
+            status: true,
+            message: 'Success',
+            data: {
+                updatebook
+            }
+});
+    } catch (err) {
+        return res.status(500).send({ msg: err.message });
+    }
+}
+
+
+module.exports = { createBooks, getallBook, updatebooks }
--- src/routes/router.js
+++ src/routes/router.js
@@ -2,12 +2,17 @@
 const express = require("express")
 const router = express.Router();
 const userController= require("../controller/userController")
+const bookController=require("../controller/bookController")
 
-//=============================== CreateUse r=======================================//
+//=============================== CreateUser=======================================//
 router.post("/register",userController.CreateUser)
 
 //=============================== Registered a book ==================================//
 router.post("/books", bookController.createBooks)
+//=============================== Get list of book ==================================//
+router.get("/books", bookController.getallBook)
+//=============================== update book ==================================//
+router.put("books/:bookId",bookController.updatebooks)
 
 module.exports = router;
 
