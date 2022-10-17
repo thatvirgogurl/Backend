2b6a7c55e1453af1c6039c7759178dae99f09821
RITIK KOHLI <ritikkohli@gmail.com>
2022-10-17 20:39:19 +0530

    cart api

---
+++ src/models/cartModel.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const ObjectId = mongoose.Schema.Types.ObjectId
+
+
+const cartSchema = new mongoose.Schema({
+    userId : {type:ObjectId, required:true, ref:'User'},
+    items: [{productId:{type:ObjectId, required:true, ref:'product'}, quantity: {type:Number,required:true}}],
+    totalPrice: {type:Number,required:true}, //Holds total price of all the items in the cart
+    totalItems: {type:Number,required:true} // Holds total number of items in the cart
+}
+,{timestamps:true}
+)
+
+module.exports = mongoose.model('cart',cartSchema)
