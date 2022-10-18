5fc7a66d0b048f232e73ac9dcdf3320c1b8c02a7
RITIK KOHLI <ritikkohli@gmail.com>
2022-10-18 10:51:24 +0530

    update cart and auhorization

--- src/controllers/cartController.js
+++ src/controllers/cartController.js
@@ -7,16 +7,7 @@ const createCart = async function(req,res){
     try{
         let data = req.body
         let userId = req.params.userId
-        // --------------- validation start ------------------
-        if(!mongoose.isValidObjectId(userId)) return res.status(400).send({status:false,message:"please provied a valid userId"})
-        let user = await userModel.findById(userId)
-        if(!user) return res.status(400).send({status:false,message:"user is not exist"})
         let {productId,cartId} = data
-        if(!productId) return res.status(400).send({status:false,message:"please provide productId"})
-        if(!mongoose.isValidObjectId(productId)) return res.status(400).send({status:false,message:"please provied a valid productId"})
-        let product = await productModel.findById(productId)
-        if(!product) return res.status(400).send({status:false,message:"product is not exist"})
-        // -----------------------------------------------------
         let cartExist = await cartModel.findOne({userId:userId})
         if(cartExist){
             // ----------------- validate cartId -----------------
@@ -25,21 +16,21 @@ const createCart = async function(req,res){
             let cart = await cartModel.findById(cartId)
             if(!cart) return res.status(400).send({status:false,message:"cart is not exist"})
             // ----------------------------------------------------
-            let productAlreadyAdded = await cartModel.findOne({"items.productId":productId})
+            let productAlreadyAdded = await cartModel.findOne({_id:cartId,"items.productId":productId})
             if(productAlreadyAdded){
-                let incProductQuantity = await cartModel.findOneAndUpdate({_id:cart._id,"items.productId":productId}
-                ,{$inc:{totalPrice:product.price,"items.$.quantity":1}},{new:true})
-                return res.status(201).send({status:true,message:"product quantity increased",data:incProductQuantity})
+                let incQuantity = await cartModel.findOneAndUpdate({_id:cartId,"items.productId":productId}
+                ,{$inc:{totalPrice:req.price,"items.$.quantity":1}},{new:true})
+                return res.status(201).send({status:true,message:"product quantity increased",data:incQuantity})
             }
             let item = {productId:productId,quantity:1}
-            let newCart = await cartModel.findByIdAndUpdate(cartExist._id,
-                {$inc:{totalPrice:product.price,totalItems:1},$push:{items:item}},{new:true})
-            return res.status(201).send({status:true,message:"product added to cart",data:newCart})
+            let productAdd = await cartModel.findByIdAndUpdate(cartId,
+                {$inc:{totalPrice:req.price,totalItems:1},$push:{items:item}},{new:true})
+            return res.status(201).send({status:true,message:"product added to cart",data:productAdd})
         }
         let cartObj = {
             userId:userId,
             items:[{productId:productId,quantity:1}],
-            totalPrice:product.price,
+            totalPrice:req.price,
             totalItems:1
         }
         let savedCart = await cartModel.create(cartObj)
@@ -50,48 +41,37 @@ const createCart = async function(req,res){
     }
 }
 
-const UpdateCart = async function(req,res){
+const updateCart = async function(req,res){
     try{
         let data = req.body
         let userId = req.params.userId
-        // --------------- validation start ------------------//
-        let {productId,cartId} = data
-        if(!productId) return res.status(400).send({status:false,message:"please provide productId"})
-        if(!mongoose.isValidObjectId(productId)) return res.status(400).send({status:false,message:"please provied a valid productId"})
-        let product = await productModel.findOne({_id:productId,isDeleted:false})
-        if(!product) return res.status(400).send({status:false,message:"product is not exist"})
-        // -----------------------------------------------------
-        let cartExist = await cartModel.findOne({userId:userId})
-        if(cartExist){
-            // ----------------- validate cartId -----------------
-            if(!cartId) return res.status(400).send({status:false,message:"please provide cartId"})
-            if(!mongoose.isValidObjectId(cartId)) return res.status(400).send({status:false,message:"please provied a valid productId"})
-            let cart = await cartModel.findById(cartId)
-            if(!cart) return res.status(400).send({status:false,message:"cart is not exist"})
-            // ----------------------------------------------------
-            let productAlreadyAdded = await cartModel.findOne({"items.productId":productId})
-            if(productAlreadyAdded){
-                let incProductQuantity = await cartModel.findOneAndUpdate({"items.productId":productId}
-                ,{$inc:{totalPrice:product.price,"items.$.quantity":1}},{new:true})
-                return res.status(201).send({status:true,message:"product quantity increased",data:incProductQuantity})
-            }
-            let item = {productId:productId,quantity:1}
-            let newCart = await cartModel.findByIdAndUpdate(cartExist._id,
-                {$inc:{totalPrice:product.price,totalItems:1},$push:{items:item}},{new:true})
-            return res.status(201).send({status:true,message:"product added to cart",data:newCart})
+        // --------------- validation start ------------------
+        let {productId,cartId,removeProduct} = data
+        if(!cartId) return res.status(400).send({status:false,message:"please provide cartId"})
+        if(!mongoose.isValidObjectId(cartId)) return res.status(400).send({status:false,message:"please provied a valid productId"})
+        let cart = await cartModel.findById(cartId)
+        if(!cart) return res.status(400).send({status:false,message:"cart is not exist"})
+        if(!removeProduct) return res.status(400).send({status:false,message:"Please provide removeProduct key"})
+        // if(removeProduct !== 0 || removeProduct !== 1) return res.status(400).send({status:false,message:"removeProduct key accept 0(to remove product) or 1(to decrement quantity)"})
+        let productInCart = await cartModel.findOne({_id:cartId,"items.productId":productId})
+        if(!productInCart) return res.status(400).send({status:false,message:"Product not in the cart"})
+        // ----------------------------------------------------
+        if(removeProduct == 0){
+            let productRemoved = await cartModel.findByIdAndUpdate(cartId,
+                {$inc:{totalPrice:-req.price,totalItems:-1},$pull:{items:{productId:productId}}},{new:true})
+            return res.status(201).send({status:true,message:"product removed from cart",data:productRemoved})
         }
-        let cartObj = {
-            userId:userId,
-            items:[{productId:productId,quantity:1}],
-            totalPrice:product.price,
-            totalItems:1
+        if(removeProduct == 1){
+            let decQuantity = await cartModel.findOneAndUpdate({_id:cartId,"items.productId":productId}
+                ,{$inc:{totalPrice:-req.price,"items.$.quantity":-1}},{new:true})
+            return res.status(201).send({status:true,message:"product quantity decreased",data:decQuantity})
         }
-        let savedCart = await cartModel.create(cartObj)
-        res.status(201).send({status:true,message:"Cart created",data:savedCart})
     }
     catch(err){
 	    res.status(500).send({status:false,message: err.message})
     }
 }
 
-module.exports = {createCart,UpdateCart}
+
+
+module.exports = {createCart,updateCart}
--- src/routes/router.js
+++ src/routes/router.js
@@ -1,32 +1,24 @@
-const express = require('express');
-const router = express.Router();
-const { authentication }=require('../middleware/auth.js')
-const { createUser, getuserById ,loginUser,updateUser} = require('../controllers/userController')
+const express = require('express')
+const router = express.Router()
+const {authentication,authorization}=require('../middleware/auth.js')
+const {createUser,getuserById,loginUser,updateUser} = require('../controllers/userController')
 const {createproduct,getProducts,getProductById,updateProduct,deleteProduct}=require('../controllers/productController')
-const {createCart} = require('../controllers/cartController')
+const {createCart,updateCart} = require('../controllers/cartController')
 
-//..............................Test API.........................//
+// ---------------------------- test API ------------------------------------- //
 
 router.get("/test-me" , function(req,res){
     res.status(200).send({status:true,message:"Testing API"})
 })
 
-
-//........................................User API............................................//
+// ---------------------------- user API ------------------------------------- //
 
 router.post('/register', createUser)
-
-
 router.get("/user/:userId/profile",authentication,getuserById)
-
 router.post('/login', loginUser)
-
 router.put("/user/:userId/profile",authentication,updateUser)
 
-
-
-
-//----------------------------------------Product API--------------------------//
+// ---------------------------- product API ---------------------------------- //
 
 router.post('/products', createproduct)
 router.get('/products', getProducts)
@@ -34,10 +26,12 @@ router.get('/products/:productId', getProductById)
 router.put("/products/:productId", updateProduct)
 router.delete('/products/:productId', deleteProduct)
 
+// ---------------------------- cart API ------------------------------------- //
 
-// ---------------------------- cart API -------------------------------------//
+router.post("/users/:userId/cart",authentication,authorization,createCart)
+router.put("/users/:userId/cart",authentication,authorization,updateCart)
 
-router.post("/users/:userId/cart",createCart)
+// --------------------------------------------------------------------------- //
 
 router.all("/*", (req, res) => { res.status(400).send({ status: false, message: "Endpoint is not correct plese provide a proper end-point" }) })
 
